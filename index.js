687eb517007b12334ea26933c389945aaa73d903
Dev Raj <vu.241fa04624@gmail.com>
2026-06-30 02:00:33 +0530

    Added Show , Create And Edit Chats Functionality

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app=express();
 const mongoose =require("mongoose");
 const path=require("path");
 const Chat =require("./models/chat.js");
+const methodOverride=require("method-override")
+app.use(express.urlencoded({extended:true}))
 
+app.use(methodOverride("_method"))
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
@@ -29,9 +32,51 @@ app.get("/chats",async(req,res)=>{
        res.render("index.ejs",{chats});
 });
 
+
+//New Route
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+//Create Route
+app.post("/chats",(req,res)=>{
+    let{from, to ,msg}=req.body
+    let newChat=new Chat({
+        from : from,
+        to: to,
+        msg: msg,
+        created_at:new Date()
+    });
+
+    newChat.save().then((res)=>{
+        console.log(res);
+    }).catch((err)=>{
+        console.log(err)
+    })
+   res.redirect("/chats")
+})
 app.get('/',(req,res)=>{
     res.send("Root is working");
 })
+
+//edit route
+app.get("/chats/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    let  chat=await Chat.findById(id);
+    res.render("edit",{chat})
+})
+//update route
+app.put("/chats/:id",async(req,res)=>{
+    let {id}=req.params;
+    let {msg:newMsg}=req.body;
+    console.log(newMsg);
+    let updatedChat=await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators:true,new:true});
+    
+    console.log(newMsg)
+    res.redirect("/chats")
+    
+})
+
 
 
 //for server starting
